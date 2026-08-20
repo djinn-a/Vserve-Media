a698704726f369df4ee71b10988322cd19ade644
Anju Vishwakarma <anjuvishwakarma1188@gmail.com>
2026-08-20 16:53:18 +0530

    scroll image reverse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 
 import { ScrollImageSequence } from "@/components/ScrollImageSequence";
+import ScrollImageSequenceReverse from "@/components/ScrollImageSequenceReverse/ScrollImageSequenceReverse";
 import DialImage from "@/features/dial-img-change/dialImage";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-black text-white selection:bg-indigo-500 selection:text-white">
-    
+
       {/* Mobile-Only Scroll-Driven Image Sequence Sticky Section */}
       <ScrollImageSequence />
-      
+
       {/* Scroll-Driven Image + Dial Section */}
       <DialImage />
 
-   
+
+      {/* Reversed Scroll-Driven Image Sequence Sticky Section */}
+      <ScrollImageSequenceReverse />
+
     </main>
   );
 }
